d51167073160970bc54c25e9f9451dde7a90e1ef
ahrian87 <maciej.zbydniewski@gmail.com>
2022-10-24 20:44:21 +0200

    dodano input użytkownika

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String name = "Maciej";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Podaj swoje imię: ");
+        String name = sc.nextLine();
         int age = 35;
         System.out.println("Hello " + name + ", mam " + age + " lat. ");
     }
